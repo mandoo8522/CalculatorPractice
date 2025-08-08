e2a09e6f0e70648a931b7e2a571f12616bcc67ba
LAPTOP-DDJROK0S\hajin <hj.kim@protnc.kr>
2025-08-08 17:02:05 +0900

    초기 커밋.

---
+++ CalculatorPractice/Model/CalculatorModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorPractice
+{
+    public class CalculatorModel
+    {
+        public double Add(double a, double b) => a + b;
+        public double Substract(double a, double b) => a - b;
+        public double Multiply(double a, double b) => a * b;
+        public double Divide(double a, double b) => b == 0 ? throw new DivideByZeroException() : a / b;
+        //public double Remainder(double a, double b) =>
+        
+       
+
+    }
+}
