27346d5d13d03ba6033904c0497443d4a4bf4d00
mandoo8522 <120428984+mandoo8522@users.noreply.github.com>
2025-08-14 16:06:57 +0900

    Add files via upload

---
+++ CalculatorPractice-master/CalculatorPractice/HistoryView.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CalculatorPractice
+{
+    /// <summary>
+    /// HistoryView.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class HistoryView : Window
+    {
+        public HistoryView(ObservableCollection<string> history,CalculatorViewModel viewModel)
+        {
+            InitializeComponent();
+            DataContext = viewModel;
+
+        }
+    }
+}
